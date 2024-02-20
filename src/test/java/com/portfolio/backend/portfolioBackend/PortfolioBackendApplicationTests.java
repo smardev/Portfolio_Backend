1ac5851c016c66d9f9e98cc874f6fd4b615eb3fc
smardev <syrine19901945@gmail.com>
2024-02-20 15:24:29 -0500

    back_1 : structure du projet

---
+++ src/test/java/com/portfolio/backend/portfolioBackend/PortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.backend.portfolioBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
